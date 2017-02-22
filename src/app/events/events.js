5a2c1e2dc45666778a6da59d78da02ec9cab293c
Cole Johnson <john1739@purdue.edu>
2017-02-22 17:56:19 -0500

    Finished up dashboard

--- src/app/events/events.js
+++ src/app/events/events.js
@@ -1,6 +1,6 @@
 module.exports = {
   template: require('./index.html'),
-  controller: function ($scope) {
+  controller: function ($scope, $cookies, $http, $location) {
     $scope.hello = 'Hello World!';
     $scope.events = [
       {
@@ -22,5 +22,28 @@ module.exports = {
         response: 'no'
       }
     ];
+
+    $scope.updateResponse = function (id, response) {
+      if (response === 'yes' || response === 'no' || response === 'maybe') {
+        var token = $cookies.get('token');
+        var postObj = {token: token, eventId: id, response: response};
+
+        $http.post("http://localhost:5000/events/response", postObj).then(function (res) {
+          if (res.data.success) {
+            for (var i = 0; i < $scope.events.length; i++) {
+              if ($scope.events[i].id === id) {
+                $scope.events[i].response = response;
+              }
+            }
+          }
+        });
+      } else {
+        console.log("Go away, Hacker!");
+      }
+    };
+
+    $scope.goToEvent = function (id) {
+      $location.path('/event/' + id);
+    };
   }
 };
