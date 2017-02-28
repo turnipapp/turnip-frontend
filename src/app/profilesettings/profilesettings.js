a380273e4ece3ccd96f617f4e5cc130b71fcc1db
Abigail Alderson <aldersoa@purdue.edu>
2017-02-27 22:09:39 -0500

    Updated profile settings

--- src/app/profilesettings/profilesettings.js
+++ src/app/profilesettings/profilesettings.js
@@ -11,5 +11,17 @@ module.exports = {
         $scope.userData = res.data;
       }
     });
+    $scope.update = function () {
+      $http.put($rootScope.url + '/account', $scope.userData, {
+        headers: {
+          token: $cookies.get('token')
+        }
+      }).then(function (res) {
+        if (res.data.success) {
+          $scope.success = "Success";
+          console.log(res.data);
+        }
+      });
+    };
   }
 };
