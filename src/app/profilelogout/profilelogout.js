aa41046da83775da848007787a0a58825578fb1e
Cole Johnson <john1739@purdue.edu>
2017-03-02 14:29:47 -0500

    Changes some profile shit

--- src/app/profilelogout/profilelogout.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  template: require('./index.html'),
-  controller: function ($scope, $stateParams) {
-    var id = $stateParams.id;
-    $scope.id = id;
-    $scope.hello = 'Hello Logout!';
-  }
-};
