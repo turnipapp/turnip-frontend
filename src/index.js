5df71955af493ae39b7e4fc56e5d9c9c08ec7074
Cole Johnson <john1739@purdue.edu>
2017-04-07 00:22:50 -0400

    Notifications

--- src/index.js
+++ src/index.js
@@ -19,6 +19,7 @@ var info = require('./app/info/info');
 var feed = require('./app/feed/feed');
 var apps = require('./app/apps/apps');
 var invite = require('./app/invite/invite');
+var notifications = require('./app/notifications/notifications');
 
 require('angular-ui-router');
 require('angular-jwt');
@@ -55,4 +56,5 @@ angular
   .component('feed', feed)
   .component('apps', apps)
   .component('invite', invite)
+  .component('notifications', notifications)
   .component('myprofile', myprofile);
