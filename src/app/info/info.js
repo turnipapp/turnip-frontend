61d404a18bb1d892d5f66f2f723b79700a77c8bd
Cole Johnson <john1739@purdue.edu>
2017-04-10 21:11:45 -0400

    Added guest tabble

--- src/app/info/info.js
+++ src/app/info/info.js
@@ -1,6 +1,30 @@
 module.exports = {
   template: require('./index.html'),
   controller: function ($scope, $http, $cookies, $stateParams, $state) {
+    $scope.setTab = function (tab) {
+      console.log(tab);
+      $scope.guestTabs.current = tab.name;
+      for (var i = 0; i < $scope.guestTabs.tabs.length; i++) {
+        if ($scope.guestTabs.tabs[i].name === tab.name) {
+          $scope.guestTabs.tabs[i].state = 'selected';
+        } else {
+          $scope.guestTabs.tabs[i].state = '';
+        }
+      }
+    };
+
+    $scope.guests = {};
+    $scope.$watch('guests', function () {});
+
+    $http.get("http://localhost:5000/event/" + $stateParams.id + "/getInviteStatus", {headers: {token: $cookies.get('token')}}).then(function (res) {
+      if (res.data.success) {
+        $scope.guests.yes = res.data.yes;
+        $scope.guests.no = res.data.no;
+        $scope.guests.maybe = res.data.maybe;
+        $scope.guests.pending = res.data.pending;
+      }
+    });
+
     $http.get("http://localhost:5000/event/" + $stateParams.id, {headers: {token: $cookies.get('token')}}).then(function (res) {
       if (res.data.success) {
         $scope.event = res.data.event;
