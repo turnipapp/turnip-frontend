a202d92ca1a9e2d9cb77615c0af16c9b43a0c713
Cole Johnson <john1739@purdue.edu>
2017-04-14 02:22:42 -0400

    Basic payment page

--- src/app/venmo/venmo.js
+++ src/app/venmo/venmo.js
@@ -4,5 +4,22 @@ module.exports = {
     var id = $stateParams.id;
     $scope.id = id;
     $scope.hello = 'Hello Profile!';
+    $scope.peoplePaid = [
+      {
+        name: 'Cole Johnson'
+      },
+      {
+        name: 'Abigail Alderson'
+      },
+      {
+        name: 'Keegan Irby'
+      },
+      {
+        name: 'Kevin Cardona'
+      },
+      {
+        name: 'Kyle Pollina'
+      }
+    ];
   }
 };
