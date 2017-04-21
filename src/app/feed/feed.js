374fc8dc7aff22950fbca6a7e327fceea9840c77
Cole Johnson <john1739@purdue.edu>
2017-04-21 01:59:58 -0400

    Adds feed

--- src/app/feed/feed.js
+++ src/app/feed/feed.js
@@ -1,6 +1,20 @@
 module.exports = {
   template: require('./index.html'),
-  controller: function ($scope) {
-    $scope.hello = 'Hello World!';
+  controller: function ($scope, $http, $cookies, $rootScope, $location) {
+    $scope.feed = [];
+
+    $http.get("http://localhost:5000/feed", {headers: {token: $cookies.get('token')}}).then(function (res) {
+      if (res.data.success) {
+        $scope.feed = res.data.feed;
+      }
+    });
+
+    $scope.goToEvent = function (id) {
+      $rootScope.eventSelected = '';
+      $rootScope.feedSelected = '';
+      $rootScope.profileSelected = '';
+      $rootScope.createSelected = '';
+      $location.path('/event/' + id);
+    };
   }
 };
